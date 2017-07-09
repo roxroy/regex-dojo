e9891fd657b025e3bd10923da6ca78e3bbaf707b
Unknown <roxroy@devicode.com>
2017-07-09 13:15:18 -0400

    Added webpack.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const path = require('path')
-const url = require('url'),
-      routes = require('./routes');
+const express = require('express');
+const path = require('path');
+const routes = require('./routes');
 
-const app = express()
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')));
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views');
 
 app.get('/', routes.index);
 app.get('/about', routes.about);
+
+app.get('/dashboard', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../public', 'dashboard.html'));
+});
+
+app.get('/train', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../public', 'train.html'));
+});
 
 app.listen(process.env.PORT || 3000);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('public'),
+    filename: 'app.js'
+  },
+  resolve: {
+     extensions: ['.jsx', '.js', '.css'],
+	},
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, 
+        loader: 'babel-loader', 
+        exclude: /node_modules/,
+        query: {
+          presets:['es2015','react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("css-loader")
+      }
+    ]
+  },
+  // Use the plugin to specify the resulting filename (and add needed behavior to the compiler)
+  plugins: [
+        new ExtractTextPlugin("app.css")
+  ],
+  devtool: "source-map"
+}
