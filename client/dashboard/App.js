46526066d39835099b76d81e9cfc1576c580b9d1
Unknown <roxroy@devicode.com>
2017-07-09 14:52:32 -0400

    Separate dashboard components.

--- client/dashboard/App.js
+++ client/dashboard/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello From React
+        Hello From React - Dashboard Module
       </div>
     );
   }
