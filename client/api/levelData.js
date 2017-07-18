58db4e6d6ec03b879df908fc9639f9275ba384bb
Unknown <roxroy@devicode.com>
2017-07-17 21:33:23 -0400

    Add levelData to api folder

---
+++ client/api/levelData.js
@@ -0,0 +1,54 @@
+var levelData = [
+  {
+    title: 'Basics',
+    description: 'In this lesson you will learn about REGEX Basics',
+    picture: 'https://cdn.pixabay.com/photo/2017/01/03/22/53/sunrise-1950873_1280.jpg',
+  },
+  {
+    title: 'Character Classes',
+    description: 'In this lesson you will learn about REGEX Character Classes',
+    picture: 'https://cdn.pixabay.com/photo/2016/03/21/10/44/desert-1270345_1280.jpg',
+  },
+  {
+    title: 'Character Classes II',
+    description: 'In this lesson you will learn about REGEX Character Classes II',
+    picture: 'https://cdn.pixabay.com/photo/2013/02/03/19/24/sunrise-77677_1280.jpg',
+  },
+  {
+    title: 'Assertions',
+    description: 'In this lesson you will learn about REGEX Assertions',
+    picture: 'https://cdn.pixabay.com/photo/2016/11/18/16/04/tree-1835529_1280.jpg',
+  },
+  {
+    title: 'Assertions II',
+    description: 'In this lesson you will learn about REGEX Assertions II',
+    picture: 'https://cdn.pixabay.com/photo/2016/05/02/17/05/nature-1367681_1280.jpg',
+  },
+  {
+    title: 'Quantifiers',
+    description: 'In this lesson you will learn about REGEX Quantifiers',
+    picture: 'https://cdn.pixabay.com/photo/2016/08/09/21/54/patagonia-1581878_1280.jpg',
+  },
+  {
+    title: 'Special Characters',
+    description: 'In this lesson you will learn about REGEX Special Characters',
+    picture: 'https://cdn.pixabay.com/photo/2014/11/13/23/33/glacier-530050_1280.jpg',
+  },
+  {
+    title: 'Special Characters II',
+    description: 'In this lesson you will learn about REGEX Special Characters II',
+    picture: 'https://cdn.pixabay.com/photo/2016/06/17/00/27/seven-sisters-1462388_1280.jpg',
+  },
+  {
+    title: 'Flags',
+    description: 'In this lesson you will learn about REGEX Flags',
+    picture: 'https://cdn.pixabay.com/photo/2016/03/19/00/59/seven-sisters-1266043_1280.jpg',
+  },       
+  {
+    title: 'Groups',
+    description: 'In this lesson you will learn about REGEX Groups',
+    picture: 'https://cdn.pixabay.com/photo/2012/01/09/11/22/the-pleiades-star-cluster-11637_1280.jpg',
+  }           
+];
+
+export default levelData;
--- client/components/App.js
+++ client/components/App.js
@@ -1,63 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router'
 import Scroll from './Scroll'
 import Level from './Level'
 import Profile from './Profile'
 import NavLink from './NavLink'
 import profileData from '../api/profileData'
-
-var levelData = [
-  {
-    title: 'Basics',
-    description: 'In this lesson you will learn about REGEX Basics',
-    picture: 'https://cdn.pixabay.com/photo/2017/01/03/22/53/sunrise-1950873_1280.jpg',
-  },
-  {
-    title: 'Character Classes',
-    description: 'In this lesson you will learn about REGEX Character Classes',
-    picture: 'https://cdn.pixabay.com/photo/2016/03/21/10/44/desert-1270345_1280.jpg',
-  },
-  {
-    title: 'Character Classes II',
-    description: 'In this lesson you will learn about REGEX Character Classes II',
-    picture: 'https://cdn.pixabay.com/photo/2013/02/03/19/24/sunrise-77677_1280.jpg',
-  },
-  {
-    title: 'Assertions',
-    description: 'In this lesson you will learn about REGEX Assertions',
-    picture: 'https://cdn.pixabay.com/photo/2016/11/18/16/04/tree-1835529_1280.jpg',
-  },
-  {
-    title: 'Assertions II',
-    description: 'In this lesson you will learn about REGEX Assertions II',
-    picture: 'https://cdn.pixabay.com/photo/2016/05/02/17/05/nature-1367681_1280.jpg',
-  },
-  {
-    title: 'Quantifiers',
-    description: 'In this lesson you will learn about REGEX Quantifiers',
-    picture: 'https://cdn.pixabay.com/photo/2016/08/09/21/54/patagonia-1581878_1280.jpg',
-  },
-  {
-    title: 'Special Characters',
-    description: 'In this lesson you will learn about REGEX Special Characters',
-    picture: 'https://cdn.pixabay.com/photo/2014/11/13/23/33/glacier-530050_1280.jpg',
-  },
-  {
-    title: 'Special Characters II',
-    description: 'In this lesson you will learn about REGEX Special Characters II',
-    picture: 'https://cdn.pixabay.com/photo/2016/06/17/00/27/seven-sisters-1462388_1280.jpg',
-  },
-  {
-    title: 'Flags',
-    description: 'In this lesson you will learn about REGEX Flags',
-    picture: 'https://cdn.pixabay.com/photo/2016/03/19/00/59/seven-sisters-1266043_1280.jpg',
-  },       
-  {
-    title: 'Groups',
-    description: 'In this lesson you will learn about REGEX Groups',
-    picture: 'https://cdn.pixabay.com/photo/2012/01/09/11/22/the-pleiades-star-cluster-11637_1280.jpg',
-  }           
-];
-
+import levelData from '../api/levelData'
 
 class App extends React.Component { 
   constructor(props) {
@@ -74,21 +22,22 @@ class App extends React.Component {
 
   render() {
     if (!this.state.loading) document.querySelectorAll(".scroll-body")[0].classList.remove("scroll-folded");
-    return <div> 
+    return <div>
         <div className="row">
           <div className="col offset-l3 offset s12 l6">
             <Profile profile={this.state.profile} />
           </div>
         </div>
         <div className="card-panel teal lighten-2">
-          <h4 className="white-text center-align"><NavLink to="/cheatsheet">Cheatsheet</NavLink>
+          <h4 className="white-text center-align">
+           <Link to={{ pathname: '/cheatsheet', state: this.state.levels }}>Cheatsheet</Link>
           </h4>
         </div>
             
         <Scroll>
           <div className="row">
             {this.state.levels.map(function(obj, i){
-              return <div className="col s12 m6">
+              return <div className="col s12 m6" key={i}>
                 <Level level={obj.title} description={obj.description} pic={obj.picture}/>
               </div>
             })}
