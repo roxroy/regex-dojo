9680ff665d6c3e35ff27ccaba008fba4dd5e7ad0
Unknown <roxroy@devicode.com>
2017-07-17 21:34:09 -0400

    Fix className property

--- client/components/Level.js
+++ client/components/Level.js
@@ -6,7 +6,7 @@ class Level extends React.Component {
         <div className="card-image waves-effect waves-block waves-light">
             <img className="activator" src={this.props.pic}/>
         </div>
-        <div claclassNames="card-content">
+        <div className="card-content">
             <span className="card-title activator grey-text text-darken-4">{this.props.level}<i className="material-icons right">more_vert</i></span>
         </div>
         <div className="card-action">
