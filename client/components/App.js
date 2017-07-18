c41e2d2c016c06dc4bcb8041e8c8667e7e869dc3
Unknown <roxroy@devicode.com>
2017-07-17 21:50:36 -0400

    Update react-router component

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 import Scroll from './Scroll'
 import Level from './Level'
 import Profile from './Profile'
--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {Router, browserHistory} from 'react-router';
-import {Route, IndexRoute} from 'react-router';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import App from './components/App'
 import Train from './components/Train'
 import Spar from './components/Spar'
@@ -10,11 +9,13 @@ import Cheatsheet from './components/Cheatsheet'
 import './components/style.css'
 
 render((
-  <Router history={browserHistory}>
+  <Router>
+  <div>
     <Route path="/app/dojo" component={App}/>
     <Route path="/train" component={Train}/>
     <Route path="/spar" component={Spar}/>
     <Route path="/fight" component={Fight}/>
     <Route path="/cheatsheet" component={Cheatsheet}/>
+  </div>
   </Router>
 ), document.getElementById('root'))
