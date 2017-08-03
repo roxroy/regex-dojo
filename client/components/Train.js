2053a335952b38113917785755e0b5333ffe4f46
Michael <michael.aldave@outlook.com>
2017-08-03 00:16:55 -0400

    Fix train to work with mongo schema

--- client/components/Train.js
+++ client/components/Train.js
@@ -25,8 +25,8 @@ class Train extends React.Component {
         <p className="flow-text text-darken-4">{this.props.level}</p>
       </div>      
       <div className="carousel" height='300px'>      
-        {this.props.levels.find((level)=>level.id === this.props.levelId).techniques.map((technique, i, arr) => {
-            return <div key={technique.id} className="center-align carousel-item teal lighten-3">                
+        {this.props.levels[this.props.levelId].techniques.map((technique, i, arr) => {
+            return <div key={i} className="center-align carousel-item teal lighten-3">                
               <h3 className='pink-text text-accent-2'>{technique.meta}</h3>
               <p className='grey-text text-darken-4'>{technique.description}</p>
               <p className='pink-text text-accent-2 carousel-footer'>{i+1}/{arr.length}</p>
